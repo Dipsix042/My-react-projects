ca6c99049891876a1854a6f21f2d3c0142b80aa3
Dipsix <dipsix123@gmail.com>
2025-06-02 11:56:44 +0100

    edit

--- src/Mycomponent7.jsx
+++ src/Mycomponent7.jsx
@@ -3,7 +3,9 @@ import React, { useState, useEffect, useRef } from "react";
 function Mycomponent7() {
   
 
-    const imputRef = useRef(null);
+    const imputRef1 = useRef(null);
+    const imputRef2 = useRef(null);
+    const imputRef3 = useRef(null);
 
     useEffect(() => {
       console.log("COMPONENT RENDERED");
@@ -11,18 +13,39 @@ function Mycomponent7() {
 
 
 
-    function handleClick() {
-       imputRef.current.focus();
+    function handleClick1() {
+       imputRef1.current.focus();
+       imputRef1.current.style.background = "yellow";
+
     }
+    function handleClick2() {
+       imputRef2.current.focus();
+       imputRef2.current.style.background = "yellow";
+
+    }
+    function handleClick3() {
+       imputRef3.current.focus();
+       imputRef3.current.style.background = "yellow";
+
+    }
+
 
 
 
     return (
       <div>
-          <button onClick={handleClick}>
-               Click Me!
+          <button onClick={handleClick1}>
+               Click Me1!
           </button>
-          <imput ref={imputRef}/>
+          <imput ref={imputRef1}/>
+          <button onClick={handleClick2}>
+               Click Me2!
+          </button>
+          <imput ref={imputRef2}/>
+          <button onClick={handleClick3}>
+               Click Me3!
+          </button>
+          <imput ref={imputRef3}/>
         </div>
     )
 
